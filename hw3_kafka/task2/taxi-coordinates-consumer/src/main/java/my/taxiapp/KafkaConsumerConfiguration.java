eaec7edc69b60e7539c395de636185a12dec9612
RobertGaz <gazizov.robert@gmail.com>
2023-10-07 01:12:14 +0400

    continue task2 of hw3

--- hw3_kafka/task2/taxi-coordinates-consumer/src/main/java/my/taxiapp/KafkaConsumerConfiguration.java
+++
@@ -1,7 +0,0 @@
-package my.taxiapp;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class KafkaConsumerConfiguration {
-}
---
+++ hw3_kafka/task2/taxi-destinations-logger/src/test/java/my/taxiapp/TaxiDestinationsLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.taxiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiDestinationsLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

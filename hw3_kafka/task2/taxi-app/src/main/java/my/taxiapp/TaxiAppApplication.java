6ab9e6f87c2003fedc1317684faf3adba994b856
RobertGaz <gazizov.robert@gmail.com>
2023-10-06 22:46:19 +0400

    started task2 of hw3

---
+++ hw3_kafka/task2/taxi-app/src/main/java/my/taxiapp/TaxiAppApplication.java
@@ -0,0 +1,13 @@
+package my.taxiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiAppApplication.class, args);
+	}
+
+}

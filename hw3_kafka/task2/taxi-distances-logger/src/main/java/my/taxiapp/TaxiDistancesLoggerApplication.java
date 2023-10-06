7b863dffaa1c03eb0415d2efba60259b207d286d
RobertGaz <gazizov.robert@gmail.com>
2023-10-07 01:19:44 +0400

    continue task2 of hw3

---
+++ hw3_kafka/task2/taxi-distances-logger/src/main/java/my/taxiapp/TaxiDistancesLoggerApplication.java
@@ -0,0 +1,13 @@
+package my.taxiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiDistancesLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiDistancesLoggerApplication.class, args);
+	}
+
+}
---
+++ hw3_kafka/task2/taxi-distances-logger/src/main/java/my/taxiapp/config/KafkaConsumerConfig.java
@@ -0,0 +1,36 @@
+package my.taxiapp.config;
+
+import org.apache.kafka.common.serialization.DoubleDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Double> consumerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("bootstrap.servers", "localhost:29092");
+        properties.put("group.id", "taxi-destination-service-group");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("key.deserializer", StringDeserializer.class);
+        properties.put("value.deserializer", DoubleDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Double> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Double> containerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        containerFactory.setConsumerFactory(consumerFactory());
+
+        return containerFactory;
+    }
+}

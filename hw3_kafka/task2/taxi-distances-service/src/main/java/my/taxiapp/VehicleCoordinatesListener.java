2f9d3ae6f91b462f22821a12a1d751e095b2b634
RobertGaz <gazizov.robert@gmail.com>
2023-10-07 20:34:23 +0400

    hw3 added

--- hw3_kafka/task2/taxi-distances-service/src/main/java/my/taxiapp/VehicleCoordinatesListener.java
+++ hw3_kafka/task2/taxi-distances-service/src/main/java/my/taxiapp/VehicleCoordinatesListener.java
@@ -6,22 +6,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class VehicleCoordinatesListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(VehicleCoordinatesListener.class);
     private static final String VEHICLE_COORDINATES_TOPIC = "vehicle_coordinates_topic";
+    private static final String VEHICLE_DISTANCES_TOPIC = "vehicle_distances_topic";
 
     @Autowired
     private VehicleDistanceService vehicleDistanceService;
 
     @KafkaListener(topics = VEHICLE_COORDINATES_TOPIC)
-    public void getVehicleCoordinates(@Payload VehicleCoordinates vehicleCoordinates,
-                                      @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
-        LOGGER.info("Received {} from partition {}", vehicleCoordinates, partition);
-        vehicleDistanceService.processVehicleCoordinates(vehicleCoordinates);
+    @SendTo(VEHICLE_DISTANCES_TOPIC)
+    public Message<Double> processCoordinates(@Payload VehicleCoordinates vehicleCoordinates,
+                                             @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+        LOGGER.info("Received {} from partition {} of topic '{}'", vehicleCoordinates, partition, VEHICLE_COORDINATES_TOPIC);
+
+        String vehicleId = vehicleCoordinates.getVehicleId();
+        Double vehicleDistance = vehicleDistanceService.calculateVehicleDistance(vehicleCoordinates);
+
+        LOGGER.info("Calculated latest distance info ({}) for vehicle '{}', sending to '{}' topic...", vehicleDistance, vehicleId, VEHICLE_DISTANCES_TOPIC);
+
+        return MessageBuilder.withPayload(vehicleDistance)
+				.setHeader(KafkaHeaders.KEY, vehicleId).build();
     }
 }
--- hw3_kafka/task2/taxi-distances-service/src/main/java/my/taxiapp/config/KafkaConsumerConfig.java
+++ hw3_kafka/task2/taxi-distances-service/src/main/java/my/taxiapp/config/KafkaConsumerConfig.java
@@ -2,11 +2,13 @@ package my.taxiapp.config;
 
 import my.taxiapp.model.VehicleCoordinates;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -15,11 +17,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Autowired
+    private KafkaTemplate<String, Double> kafkaTemplate; //defined in KafkaProducerConfig
+
     @Bean
     public ConsumerFactory<String, VehicleCoordinates> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
         properties.put("bootstrap.servers", "localhost:29092");
-        properties.put("group.id", "taxi-destinations-service-group");
+        properties.put("group.id", "taxi-distances-service-group");
         properties.put("auto.offset.reset", "earliest");
 
         return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), new JsonDeserializer<>(VehicleCoordinates.class));
@@ -29,7 +34,7 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, VehicleCoordinates> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, VehicleCoordinates> containerFactory = new ConcurrentKafkaListenerContainerFactory<>();
         containerFactory.setConsumerFactory(consumerFactory());
-
+        containerFactory.setReplyTemplate(kafkaTemplate);
         return containerFactory;
     }
 }
